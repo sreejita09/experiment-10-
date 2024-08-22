5c6d6887749379fe3c0b4e258079e27d421a8fb5
Sreejita Bhardwaj <sreejitabhardwaj2005@gmail.com>
2024-08-22 22:48:28 +0530

    Add files via upload

---
+++ call_by_value_2.cpp
@@ -0,0 +1,23 @@
+// Sreejita Bhadrdwaj 
+// 23070123130
+// ENTC B2 
+// experiment 10
+
+#include<iostream> 
+using namespace std; 
+void swap(int *x, int *y) 
+{
+    int *swap;
+    swap=x;
+    x=y;
+    y=swap;
+}
+
+int main() 
+{
+    int a=4,b=7;
+    swap(a,b);
+    cout<<"Value of a is: "<<a<<"\n";
+    cout<<"Value of b is: "<<b<<"\n";
+    return 0;
+}
